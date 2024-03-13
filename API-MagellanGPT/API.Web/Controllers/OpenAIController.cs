14af3010626139be5b067fd773e11609c723f79c
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2024-03-13 11:35:32 +0100

    implémentation de semantic kernel

---
+++ API-MagellanGPT/API.Web/Controllers/OpenAIController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace API.Web.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIController : ControllerBase
+    {
+        private Kernel kernel;
+        
+        public OpenAIController(Kernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        [HttpGet]
+        public async IAsyncEnumerable<string> Chat(string userMessage) 
+        {
+            var chat = kernel.Services.GetRequiredKeyedService<IChatCompletionService>("gpt3");
+
+            var chatHistory = new ChatHistory();
+
+            chatHistory.AddUserMessage(userMessage);
+
+            await foreach (var item in chat.GetStreamingChatMessageContentsAsync(chatHistory))
+            {
+                yield return item.Content;
+            }
+        }
+    }
+}
--- API-MagellanGPT/API.Web/Program.cs
+++ API-MagellanGPT/API.Web/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.SemanticKernel;
+
 namespace API.Web
 {
     public class Program
@@ -13,6 +15,10 @@ namespace API.Web
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddKernel()
+                .AddAzureOpenAIChatCompletion("gpt-35", "https://magellan-gpt.openai.azure.com/", "10306409e1964dd994d7be5a04daf638", serviceId:"gpt3")
+                .AddAzureOpenAIChatCompletion("gpt-4", "https://magellan-gpt.openai.azure.com/", "10306409e1964dd994d7be5a04daf638", serviceId:"gpt4");
 
             var app = builder.Build();
 
