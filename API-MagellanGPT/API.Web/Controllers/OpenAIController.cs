595f54c4eac910d2c5b267ba775c7e637acdcccd
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2024-03-18 11:42:30 +0100

    RAG : en cours de création du Bolb

--- API-MagellanGPT/API.Web/Controllers/OpenAIController.cs
+++ API-MagellanGPT/API.Web/Controllers/OpenAIController.cs
@@ -1,7 +1,10 @@
 ﻿using API.Application.Common.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using System.IO;
+using System.Reflection.Metadata;
 
 namespace API.Web.Controllers
 {
@@ -27,6 +30,50 @@ namespace API.Web.Controllers
 
             chatHistory.AddUserMessage(userMessage.RequestMessage);
 
+            await foreach (var item in chat.GetStreamingChatMessageContentsAsync(chatHistory))
+            {
+                yield return item.Content;
+            }
+        }
+
+        [HttpPost("RAG")]
+        public async IAsyncEnumerable<string> Rag([FromForm] RagDto userMessage)
+        {
+            // Vérifiez si des fichiers ont été envoyés
+            if (userMessage.Files == null || !userMessage.Files.Any())
+            {
+                yield return "No files were provided.";
+                yield break;
+            }
+
+            // Récupérez les documents pertinents à partir des fichiers envoyés
+            var relevantDocuments = new List<object>();
+            foreach (var file in userMessage.Files)
+            {
+                if (file.Length > int.MaxValue)
+                {
+                    yield return "File is too large.";
+                    yield break;
+                }
+
+                byte[] blob;
+                using (var memoryStream = new MemoryStream())
+                {
+                    await file.CopyToAsync(memoryStream);
+                    blob = memoryStream.ToArray();
+                }
+            }
+
+            // Ajoutez les documents récupérés à l'historique de la conversation
+            var chatHistory = new ChatHistory();
+            chatHistory.AddUserMessage(userMessage.RequestMessage);
+            foreach (var document in relevantDocuments)
+            {
+                chatHistory.AddSystemMessage(document);
+            }
+
+            // Utilisez le modèle de langage pour générer une réponse en fonction des documents récupérés
+            var chat = kernel.Services.GetRequiredKeyedService<IChatCompletionService>(userMessage.Model);
             await foreach (var item in chat.GetStreamingChatMessageContentsAsync(chatHistory))
             {
                 yield return item.Content;
