45fd943995bcf687f5aa585a92614017357297eb
Clément Delarue <clement.delarue@saint-benigne.fr>
2024-03-20 09:38:36 +0100

    feature : prompt system

---
+++ API-MagellanGPT/API.Application/Common/Dto/MessageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Application.Common.Dto
+{
+    public class MessageDto
+    {
+        public string? Role { get; set; }
+        public string? Content { get; set; }
+    }
+}
--- API-MagellanGPT/API.Web/Controllers/OpenAIController.cs
+++ API-MagellanGPT/API.Web/Controllers/OpenAIController.cs
@@ -18,7 +18,7 @@ namespace API.Web.Controllers
             this.kernel = kernel;
         }
 
-        [HttpPost]
+        [HttpPost("Chat")]
         public async IAsyncEnumerable<string> Chat([FromBody] RequestDto userMessage) 
         {
             var chat = kernel.Services.GetRequiredKeyedService<IChatCompletionService>(userMessage.Model);
@@ -26,6 +26,34 @@ namespace API.Web.Controllers
             var chatHistory = new ChatHistory(); // a revoir pour l'historique
 
             chatHistory.AddUserMessage(userMessage.RequestMessage);
+
+            await foreach (var item in chat.GetStreamingChatMessageContentsAsync(chatHistory))
+            {
+                yield return item.Content;
+            }
+        }
+
+        [HttpPost("ChatWithPromptSystem")]
+        public async IAsyncEnumerable<string> ChatWithPromptSystem([FromBody] MessageByRoleDto userMessage)
+        {
+            var chat = kernel.Services.GetRequiredKeyedService<IChatCompletionService>(userMessage.Model);
+
+            var chatHistory = new ChatHistory();
+
+            if (userMessage.Messages != null && userMessage.Messages.Any())
+            {
+                foreach (var message in userMessage.Messages)
+                {
+                    if (message.Role == "System")
+                    {
+                        chatHistory.AddSystemMessage(message.Content);
+                    }
+                    else if (message.Role == "User")
+                    {
+                        chatHistory.AddUserMessage(message.Content);
+                    }
+                }
+            }
 
             await foreach (var item in chat.GetStreamingChatMessageContentsAsync(chatHistory))
             {
